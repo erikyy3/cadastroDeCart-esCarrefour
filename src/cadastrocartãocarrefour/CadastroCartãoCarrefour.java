a6230369277e76d100060e4bf21606f119a66654
Erick Brandenburg <erickholtz2015@gmail.com>
2026-05-25 01:00:41 -0300

    Criação do Diagram DER do banco de dados

--- src/cadastrocartãocarrefour/CadastroCartãoCarrefour.java
+++ src/cadastrocartãocarrefour/CadastroCartãoCarrefour.java
@@ -1,12 +1,15 @@
 
 package cadastrocartãocarrefour;
 
+import views.CadastroCartao;
+
 
 public class CadastroCartãoCarrefour {
 
 
     public static void main(String[] args) {
-        
+        CadastroCartao telaCadastro = new CadastroCartao();
+        telaCadastro.setVisible(true);
     }
     
 }
